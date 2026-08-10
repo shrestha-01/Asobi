266789e15f6618ec77ffcd9c8888b278eb99569a
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-10 20:29:51 +0545

    redirect start btn to username page

--- index_script.js
+++ index_script.js
@@ -1,8 +1,14 @@
+var startBtn = document.getElementById("startBtn");
 const logoLetters = document.querySelectorAll(".logo span");
 
+//hover sound
 logoLetters.forEach(function(letter){
     letter.addEventListener("mouseenter",function(){
         let sound = new Audio("Sounds/pop.wav");
         sound.play();
     });
+});
+//redirecting to username.html
+startBtn.addEventListener("click",function(){
+    window.location.href = "username.html";
 });
