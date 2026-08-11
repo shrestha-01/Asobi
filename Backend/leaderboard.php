17ec0e27e6bcd461255deef1d1cb69be6b1b5737
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-11 20:00:22 +0545

    adding guess the number leaderbaord

---
+++ Backend/leaderboard.php
@@ -0,0 +1,21 @@
+<?php
+require "db.php";
+$difficulty = $_GET["difficulty"];
+$sql="SELECT players.username, guess_the_number_scores.score, guess_the_number_scores.created_at
+FROM guess_the_number_scores
+JOIN players
+ON guess_the_number_scores.player_id = players.id
+WHERE difficulty = ?
+ORDER BY score ASC
+LIMIT 100";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $difficulty);
+$stmt->execute();
+$result = $stmt->get_result();
+$scores = [];
+while($row = $result->fetch_assoc()){
+    $scores[]= $row;
+}
+echo json_encode($scores);
+?>
