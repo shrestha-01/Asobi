cbf8176c10ad8f4683dd2caa2267df0ae9dc4164
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-13 16:57:07 +0545

    alignment added in the leaderbaord

--- leaderboard_script.js
+++ leaderboard_script.js
@@ -1,18 +1,48 @@
 //getting elements
 var leaderTabel = document.getElementById("leaderTabel");
+var diffs = document.getElementsByClassName("diff");
 
-fetch("Backend/total_Board.php")
-    .then(function (response){
-        return response.json();
+//showing players on the leaderbaord
+function showBoard(players){
+    var header = leaderTabel.querySelector(".tRow");
+    leaderTabel.innerHTML = "";
+    leaderTabel.appendChild(header);
+
+    for(var i =0; i<players.length; i++){
+        var row = document.createElement("div");
+        row.className = "tRow";
+        row.innerHTML = '<span class="pRank">'+(i+1)+'</span>'
+        + '<span class="pName:">' + players[i].username + '</span>'
+        +'<span class="totalBox">' + players[i].score + '</span>'
+        +'<span class="pTime">' + players[i].games+'</span>';
+        leaderTabel.appendChild(row);
+    }
+}
+//loading total leaderboard
+function loadTotal(){
+    fetch("Backend/total_Board.php")
+    .then(function(r){
+        return r.json();
     })
-    .then(function (players){
-        for(var i = 0; i<players.length; i++){
-            var row = document.createElement("div");
-            row.className = "tRow";
-            row.innerHTML = '<span class="pRank">'+(i+1)+'</span>'
-            + '<span class="pName">' + players[i].username + '</span>'
-            + '<span class="totalBox">' + players[i].score + '</span>'
-            + '<span class="pTime">' + players[i].games + '</span>';
-            leaderTabel.appendChild(row);
+    .then(function(players){
+        showBoard(players);
+    });
+}
+loadTotal();
+for(var i=0; i<diffs.length; i++){
+    diffs[i].addEventListener("click",function(){
+        var text = this.textContent;
+        if(text == "Total Leaderboard"){
+            loadTotal();
+        } else {
+            var difficulty = text.split(" ")[0].toLowerCase();
+            fetch("Backend/leaderboard.php?difficulty=best_"+difficulty)
+            .then(function(r){
+                return r.json();
+            })
+            .then(function(scores){
+                showBoard(scores);
+            });
         }
     });
+}
