44cd0ba72d4a20390d3b9883b3a88b5f066cf022
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-19 11:25:14 +0545

    how score works card added

--- leaderboard_script.js
+++ leaderboard_script.js
@@ -8,6 +8,11 @@ var yourRank = document.getElementById("yourRank");
 var yourScore = document.getElementById("yourScore");
 var yourGames = document.getElementById("yourGames");
 var backBtn = document.getElementById("backBtn");
+var scoreBtn = document.getElementById("scoreBtn");
+var scorePopup = document.getElementById("scorePopup");
+var closeBtn = document.getElementById("closeBtn");
+var card= document.getElementById("scorePopupCard");
+var bounds;
 
 //showing players on the leaderbaord
 function showBoard(players){
@@ -83,4 +88,34 @@ for(var i=0; i<diffs.length; i++){
 }
 backBtn.addEventListener("click",function(){
     window.location.href = "guess.html";
+});
+//showing card for how scoring works
+scoreBtn.addEventListener("click",function(){
+    scorePopup.style.display="flex";
+});
+closeBtn.addEventListener("click",function(){
+    scorePopup.style.display="none";
+});
+
+// card design by mark miro (credited with link in github)
+function rotateToMouse(e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+    var leftX = mouseX - bounds.x;
+    var topY = mouseY - bounds.y;
+    var centerX = leftX - bounds.width / 2;
+    var centerY = topY - bounds.height / 2;
+    var distance = Math.sqrt(centerX ** 2 + centerY ** 2);
+
+    card.style.transform = "scale3d(1.05, 1.05, 1.05) rotate3d(" + (centerY / 100) + "," + (-centerX / 100) + ",0," + (Math.log(distance) * 2) + "deg)";
+
+    card.querySelector(".glow").style.backgroundImage = "radial-gradient(circle at " + (centerX * 2 + bounds.width / 2) + "px " + (centerY * 2 + bounds.height / 2) + "px, #ffffff55, #0000000f)";
+}
+card.addEventListener("mouseenter", function () {
+    bounds = card.getBoundingClientRect();
+    document.addEventListener("mousemove", rotateToMouse);
+});
+card.addEventListener("mouseleave", function () {
+    document.removeEventListener("mousemove", rotateToMouse);
+    card.style.transform = "";
 });
