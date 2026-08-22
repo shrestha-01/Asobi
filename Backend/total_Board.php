84f1f7d9ee7063dad4abb5134cd91d92f08d2810
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-22 08:02:14 +0545

    added time range filtering

--- Backend/total_Board.php
+++ Backend/total_Board.php
@@ -1,9 +1,18 @@
 <?php
 require "db.php";
+$time = $_GET["time"];
+$where = "";
+
+if($time == "24h"){
+    $where = "WHERE guess_the_number_scores.created_at >= NOW() - INTERVAL 1 DAY";
+} else if($time == "week"){
+    $where = "WHERE guess_the_number_scores.created_at >= NOW() - INTERVAL 7 DAY";
+}
+
 $sql = "SELECT players.username, SUM(guess_the_number_scores.score) AS score, COUNT(*) AS games
 FROM guess_the_number_scores
 JOIN players
-ON guess_the_number_scores.player_id = players.id
+ON guess_the_number_scores.player_id = players.id " . $where . "
 GROUP BY players.id
 ORDER BY score DESC
 LIMIT 100";
