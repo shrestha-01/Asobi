e068e639b0d486798a6b11038e1beae50407b9ba
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-02 15:20:50 +0545

    count click within time only

--- Games/the_clicker_script_01.js
+++ Games/the_clicker_script_01.js
@@ -7,9 +7,11 @@ var cps = document.getElementById("CPS");
 var timeremain;
 var timer;
 var clickno = 0;
+var timesup = true;
 
 //starting the game 
 startBtn.addEventListener("click",function(){
+    timesup = false;
     timeremain = 10;
     time.textContent = timeremain;
     timer = setInterval(countdown, 1000);
@@ -19,10 +21,15 @@ function countdown(){
     time.textContent = timeremain;
     if(timeremain <= 0){
         clearInterval(timer);
+        timesup = true;
     }
 }
 // counting cliks
 clickMe.addEventListener("click", function(){
-    clickno = clickno + 1;
-    totalClks.textContent = clickno;
+    // clickno = clickno + 1;
+    // totalClks.textContent = clickno;
+    if(!timesup){
+        clickno = clickno + 1;
+        totalClks.textContent = clickno;
+    }
 });
