4a4cd12bf3ae16467d571608bbb0b0b7b4cb3b56
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-02 15:13:20 +0545

    added countdown timer to start button

--- Games/the_clicker_script_01.js
+++ Games/the_clicker_script_01.js
@@ -4,3 +4,19 @@ var time = document.getElementById("time");
 var clickMe = document.getElementById("clickMe");
 var totalClks = document.getElementById("totalClks");
 var cps = document.getElementById("CPS");
+var timeremain;
+var timer;
+
+//starting the game 
+startBtn.addEventListener("click",function(){
+    timeremain = 10;
+    time.textContent = timeremain;
+    timer = setInterval(countdown, 1000);
+});
+function countdown(){
+    timeremain = timeremain - 1;
+    time.textContent = timeremain;
+    if(timeremain <= 0){
+        clearInterval(timer);
+    }
+}
