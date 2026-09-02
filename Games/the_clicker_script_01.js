fdff2141bae3edbbe106bf06b8b1e6a7a388a93f
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-02 15:05:25 +0545

    getting elements only

---
+++ Games/the_clicker_script_01.js
@@ -0,0 +1,6 @@
+//getting elements 
+var startBtn = document.getElementById("startBtn");
+var time = document.getElementById("time");
+var clickMe = document.getElementById("clickMe");
+var totalClks = document.getElementById("totalClks");
+var cps = document.getElementById("CPS");
