6d48be7feb6996fab63b81784b44cba28c76e374
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-02 15:36:37 +0545

    merged start and click btn

--- Games/the_clicker_script_01.js
+++ Games/the_clicker_script_01.js
@@ -1,5 +1,5 @@
 //getting elements 
-var startBtn = document.getElementById("startBtn");
+// var startBtn = document.getElementById("startBtn");
 var time = document.getElementById("time");
 var clickMe = document.getElementById("clickMe");
 var totalClks = document.getElementById("totalClks");
@@ -8,28 +8,57 @@ var timeremain;
 var timer;
 var clickno = 0;
 var timesup = true;
+var rest;
 
 //starting the game 
-startBtn.addEventListener("click",function(){
-    timesup = false;
-    timeremain = 10;
-    time.textContent = timeremain;
-    timer = setInterval(countdown, 1000);
-});
-function countdown(){
-    timeremain = timeremain - 1;
-    time.textContent = timeremain;
-    if(timeremain <= 0){
-        clearInterval(timer);
-        timesup = true;
-    }
-}
-// counting cliks
-clickMe.addEventListener("click", function(){
-    // clickno = clickno + 1;
-    // totalClks.textContent = clickno;
-    if(!timesup){
+// startBtn.addEventListener("click",function(){
+//     timesup = false;
+//     timeremain = 10;
+//     time.textContent = timeremain;
+//     timer = setInterval(countdown, 1000);
+// });
+// function countdown(){
+//     timeremain = timeremain - 1;
+//     time.textContent = timeremain;
+//     if(timeremain <= 0){
+//         clearInterval(timer);
+//         timesup = true;
+//     }
+// }
+// // counting cliks
+// clickMe.addEventListener("click", function(){
+//     // clickno = clickno + 1;
+//     // totalClks.textContent = clickno;
+//     if(!timesup){
+//         clickno = clickno + 1;
+//         totalClks.textContent = clickno;
+//     }
+// });
+
+clickMe.addEventListener("click",function(){
+    if(timesup){
+        timesup = false;
+        timeremain = 10;
+        time.textContent = timeremain;
+        timer = setInterval(countdown, 1000);
+        clickno = 1;
+        totalClks.textContent = clickno;
+    } else {
         clickno = clickno + 1;
         totalClks.textContent = clickno;
     }
 });
+function countdown(){
+    timeremain = timeremain - 1;
+    time.textContent = timeremain;
+    if(timeremain <=0){
+        clearInterval(timer);
+        timesup = true;
+        clickMe.disabled = true;
+        clickMe.textContent = "Take a breath";
+        rest = setTimeout(function(){
+            clickMe.disabled = false;
+            clickMe.textContent = "Click Me!";
+        }, 3000);
+    }
+}
